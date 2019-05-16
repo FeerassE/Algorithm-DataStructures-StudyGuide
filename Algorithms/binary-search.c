74bfbd1c094e46b6fde57f4fd5dc516299b9733f
Feerass Ellid <feerasse@gmail.com>
2019-05-16 15:52:58 -0400

    added binary search in C

--- Algorithms/binary-search.c
+++ Algorithms/binary-search.c
@@ -2,7 +2,29 @@
 #include<stdio.h>
 
 
+
+int BinarySearch(int array[], int low, int high, int target);
+
+
 int main(void){
 
 
+}
+
+int BinarySearch(int array[], int low, int high, int target){
+    if(high >= 1){
+        int mid = (low + high)/2;
+        if(array[mid] == target){
+            return mid;
+        }
+        else if(array[mid] > target){
+            return BinarySearch(array, low, mid - 1, target);
+        }
+        else if(array[mid] < target){
+            return BinarySearch(array, mid + 1, high, target);
+        }
+    }
+    else{
+        return -1;
+    }
 }
